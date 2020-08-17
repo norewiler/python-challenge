da5be4c11d0d0411cc242356b50068febd5f6daa
norewiler <n.orewiler1@gmail.com>
2020-08-17 15:09:40 -0500

    open file and count total votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,4 +2,14 @@ import os
 import csv
 
 
-#Get Started
+file_path = os.path.join("Resources", "Homework_03-Python_Instructions_PyPoll_Resources_election_data.csv")
+
+tot_votes = 0
+with open(file_path, "r") as data:
+    reader = csv.reader(data, delimiter = ",")
+    header = next(reader)
+    for row in reader:
+        tot_votes += 1
+
+
+print(tot_votes)
