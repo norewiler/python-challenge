592a35ac13032e26d1ac703a4782822611f5bcaa
norewiler <n.orewiler1@gmail.com>
2020-08-17 14:13:22 -0500

    greatest inc and dec calc'd and printed. (Still need to format as $)

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,7 +6,9 @@ file_path = os.path.join("Resources", "Homework_03-Python_Instructions_PyBank_Re
 
 num_months = 0
 totalPNL = 0
-changes = 0.0
+totalChanges = 0.0
+greatInc = 0
+greatDec = 0
 with open(file_path, "r") as data:
     reader = csv.reader(data, delimiter = ",")
     header = next(reader)
@@ -16,12 +18,21 @@ with open(file_path, "r") as data:
         if num_months == 1:
             prev = pnl
         else:
-            changes += (pnl - prev)
+            change = pnl - prev
+            totalChanges += (change)
+            if change > greatInc:
+                greatInc = change
+                greatIncMonth = row[0]
+            elif change <greatDec:
+                greatDec = change
+                greatDecMonth = row[0]
             prev = pnl
         totalPNL += int(row[1])
-avgChange = changes / (num_months - 1)
+avgChange = totalChanges / (num_months - 1)
 
 print(f"Total Months: {num_months}")
 print(f"Total: ${pnl}")
 print(f"Average Change: $ {avgChange}")
+print(f"Greates Increase in Profits: {greatIncMonth} (${greatInc})")
+print(f"Greates Decrease in Profits: {greatDecMonth} (${greatDec})")
 
