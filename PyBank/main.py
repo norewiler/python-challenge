914d119997b0544605c330ddd1fe7a80738ad3e4
norewiler <n.orewiler1@gmail.com>
2020-08-17 13:54:03 -0500

    calc and print PnL

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,13 +5,15 @@ import csv
 file_path = os.path.join("Resources", "Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv")
 
 num_months = 0
+pnl = 0
 with open(file_path, "r") as data:
     reader = csv.reader(data, delimiter = ",")
     header = next(reader)
     for row in reader:
         num_months += 1
+        pnl += int(row[1])
 
 
-
-print(num_months)
+print(f"Total Months: {num_months}")
+print(f"Total: ${pnl}")
 
