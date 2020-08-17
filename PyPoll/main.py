f962233b758bc03dae30e07a6c87dd5e26d61efb
norewiler <n.orewiler1@gmail.com>
2020-08-17 15:30:30 -0500

    totals and % calc'd and printed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,11 +5,22 @@ import csv
 file_path = os.path.join("Resources", "Homework_03-Python_Instructions_PyPoll_Resources_election_data.csv")
 
 tot_votes = 0
+results = {}
 with open(file_path, "r") as data:
     reader = csv.reader(data, delimiter = ",")
     header = next(reader)
     for row in reader:
         tot_votes += 1
+        candidate = row[2]
+        if candidate in results.keys():
+            results[candidate] += 1
+        else:
+            results[candidate] = 1
 
-
-print(tot_votes)
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {tot_votes}")
+print("-------------------------")
+for cand, votes in results.items():
+    print(f"{cand}: {round(((votes/tot_votes)*100),3)}% ({votes})")
+print("-------------------------")
