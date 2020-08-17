234cb86cc98959c6f4b0721de5c647b333242894
norewiler <n.orewiler1@gmail.com>
2020-08-17 13:40:51 -0500

    read file and count months(rows)

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,4 +2,16 @@ import os
 import csv
 
 
-#Get Started
+file_path = os.path.join("Resources", "Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv")
+
+num_months = 0
+with open(file_path, "r") as data:
+    reader = csv.reader(data, delimiter = ",")
+    header = next(reader)
+    for row in reader:
+        num_months += 1
+
+
+
+print(num_months)
+
